f653d22835271e0f0eb691ea91de2fe8922b281e
alexrider94 <phila1@naver.com>
2021-06-10 22:24:43 +0900

    Add Detail Design in Drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,4 +1,4 @@
-import { Drawer, Grid, makeStyles, Paper } from "@material-ui/core"
+import { Avatar, Card, CardContent, Divider, Drawer, Grid, IconButton, List, ListItem, ListItemIcon, ListItemText, makeStyles, Paper, Typography } from "@material-ui/core"
 import React from "react"
 import { User } from "../types/user/user";
 import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
@@ -10,10 +10,17 @@ export interface CustomDrawerProps {
 
 const useStyles = makeStyles((theme)=>({
   drawer: {
-    width: 600,
+    width: 400,
     height: '100%',
     padding: theme.spacing(1),
     background: '#A562C9',
+    color: '#ffffff'
+  },
+  userInfo: {
+    padding: theme.spacing(1)
+  },
+  currentActivity: {
+    padding: theme.spacing(1)
   }
 }))
 
@@ -29,12 +36,61 @@ export const CustomDrawer = (props: CustomDrawerProps) => {
   return <Drawer anchor={'right'}
   open={isOpen} >
     <Grid className={classes.drawer}container direction={'column'}>
-      <Paper elevation={1}>
-      <Grid item>
-        <CancelOutlinedIcon />
+      <Grid item container
+      direction='row' justify='space-between'
+      alignItems='center'
+      className={classes.userInfo}>
+        <Grid item>
+          <Typography variant='subtitle2'>
+          Your Information
+          </Typography>
+        </Grid>
+         <Grid item>
+           <IconButton>
+             <CancelOutlinedIcon />
+           </IconButton>
+         </Grid>
       </Grid>
-      <Grid item></Grid>
-      </Paper>
+      <Grid item container direction='row' spacing={3}>
+        <Grid item>
+          <Avatar />
+        </Grid>
+        <Grid item>
+          <Typography>User ID</Typography>
+          <Typography>User Name</Typography>
+        </Grid>
+
+      </Grid>
+      <Divider light variant='fullWidth'/>
+      <Grid item className={classes.currentActivity}>
+        <Typography variant='subtitle2' gutterBottom>
+        Current Activity
+        </Typography>
+        <Grid container direction='column' spacing={2}>
+          <Grid item>
+            <Card>
+              <CardContent>
+                User Name has been joined Rise of Memo
+              </CardContent>
+            </Card>
+          </Grid>
+           <Grid item>
+            <Card>
+              <CardContent>
+                User Name added new memo in Todo
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card>
+              <CardContent>
+                User Name deleted new memo in Todo
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
+
 
     </Grid>
   </Drawer>
